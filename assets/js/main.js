8d2fdecd7858ac38909e19e8f6302cea9e30ed41
Dhani Dwi Putra <dhanidwiputra371@gmail.com>
2025-12-30 10:38:39 +0700

    Menambahkan file ke github

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+(function($) {
+    "use strict";
+
+    $(document).ready( function() {
+        //>> Body Overlay Js Start <<//
+        $(".body-overlay").on("click", function() {
+            $(".offcanvas__area").removeClass("offcanvas-opened");
+            $(".df-search-area").removeClass("opened");;
+            $(".body-overlay").removeClass("opened");
+        });
+
+        //>> Sticky Header Js Start <<//
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 250) {
+                $("#header-sticky").addClass("sticky");
+            } else {
+                $("#header-sticky").removeClass("sticky");
+            }
+        });
+        
+        //>> Counterup Start <<//
+        $(".count").counterUp({
+            delay: 15,
+            time: 4000,
+        });
+
+        //>> Wow Animation Start <<//
+        new WOW().init();
+
+
+        //>> Back To Top Slider Start <<//
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 20) {
+                $("#back-top").addClass("show");
+            } else {
+                $("#back-top").removeClass("show");
+            }
+        });
+        $("#back-top").click(function () {
+            $("html, body").animate({ scrollTop: 0 }, 800);
+            return false;
+        });
+
+
+    }); // End Document Ready Function
+   
+})(jQuery); // End jQuery
+
